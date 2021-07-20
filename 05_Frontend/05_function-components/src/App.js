d3e25d281c9a4727d67947eb0419c84ffd562d6b
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-07-19 21:50:04 -0500

    Añadí un subtitulo para hacer prueba de branch

--- 05_Frontend/05_function-components/src/App.js
+++ 05_Frontend/05_function-components/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>👋 ¡Hola desde mi APP! 📲</h1>      
+      <h1>👋 ¡Hola desde mi APP! 📲</h1>  
+      <h2>Subtitulo para prueba de branch 🌵</h2>    
     </div>
   );
 }
