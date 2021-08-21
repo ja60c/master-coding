64a632fd8713b6919038012096036d0830dfd1e4
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-08-20 23:26:15 -0500

    ejercicios de repaso de la consola

---
+++ 02_JavaScript/01_IntroJS/consola/retos/retos.js
@@ -0,0 +1,23 @@
+// Impresión en consola
+console.log("Hola mundo! 👋🌎");
+console.log(5*10);
+console.log(232<234);
+console.log(typeof "Hola mundo, de nuevo!");
+
+// Variables
+var soyUnaVariableVar = 121
+let soyUnaVariableLet = 444
+const soyUnaVariableConst = 12
+
+// Uso de operadores aritméticos
+console.log(soyUnaVariableLet+soyUnaVariableConst);
+console.log(soyUnaVariableConst<soyUnaVariableVar);
+console.log(soyUnaVariableConst-soyUnaVariableConst);
+console.log(22 % 3);
+
+// Comparativos 
+const num = 15
+console.log(num > 3);
+console.log(num == 15);
+console.log(num === "15");
+
