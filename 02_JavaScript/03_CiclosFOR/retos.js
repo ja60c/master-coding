4e73f595968dd8ce4c1d6d9aa92e5a13d26ee6ce
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-08-21 22:17:29 -0500

    Realicé los retos y me salieron todos!

--- 02_JavaScript/03_CiclosFOR/retos.js
+++ 02_JavaScript/03_CiclosFOR/retos.js
@@ -8,47 +8,36 @@ console.log('💛💙💚¡RETOS!🧡💜💚');
 // 4) Pozole
 // 5) Barbacoa
 // 6) Guacamole
-//
-// 1. Mediante un ciclo FOR, muestra todos los platillos (recuerda la propiedad length)
-// const platillos = [
-//     'Enchiladas',
-//     'Taco',
-//     'Tamal',
-//     'Pozole',
-//     'Barbacoa',
-//     'Guacamole'
-// ]
-// console.log(platillos);
+let platillos = ['enchiladas', 'taco', 'tamal', 'pozole', 'barbacoa', 'guacamole']
 
-// for (let i = 0; i < platillos.length; i++){
-//     console.log(i + ' ' + platillos[i]);
+// 1. Mediante un ciclo FOR, muestra todos los platillos (recuerda la propiedad length) 
+// for (let i = 0; i <= platillos.length; i++) {
+//     console.log(platillos[i]);
+    // console.log(i + ' ' + platillos[i]); // este los enumera
+// } // ¿Cómo eliminar el undefined del final?
+
+// 2. Mediante un ciclo FOR y condicional IF  muestra sólo el Pozole y la Barbacoa
+// for (let i = 0; i <= platillos.length; i++) {
+//     // console.log(platillos[i])
+//     if (i == 3 || i == 4) {
+//         // console.log(platillos[i]) 
+//         console.log(i + ' ' + platillos[i]) 
+//     } 
 // }
-
-
-// 2. Mediante un ciclo FOR y condicional IF, muestra sólo el Pozole y la Barbacoa
-// for (let i = 0; i < platillos.length; i++) {
-//     if (i === 3 || i === 4){
-//         console.log(i + ' ' + platillos[i]);
-//     }
-// }
-
 
 // 3. Mediante un ciclo FOR y condicional IF, muestra sólo los elementos impares
-// for (let i = 0; i < platillos.length; i++) {
-//     if (i % 2 === 1){
-//         console.log(i + ' ' + platillos[i] + ' es IMPAR');
-//     } else{
-//         console.log(i + ' ' + platillos[i] + ' es PAR');
+// for (let i = 0; i <= platillos.length; i++) {  
+//     if(i % 2 == 1){
+//         console.log(i + ' ' + platillos[i] + ' ES IMPAR 👍')
+//     } else {
+//         console.log(i + ' ' + platillos[i] + ' ES PAR')
 //     }
 // }
 
-
 // 4. Mediante únicamente un ciclo FOR, muestra los elementos que sean pares
-// for (let i = 0; i < platillos.length; i += 2) {
-//    console.log(i + ' ' + platillos[i] + ' es PAR');
+// for (let i = 0; i <= platillos.length; i += 2) {
+//     console.log(i + ' ' + platillos[i] + ' Soy PAR 😀')    
 // }
-
-
 
 //RETO 2 FIZZBUZZ
 // Mostrar en consola los números del 1 al 100
@@ -69,18 +58,15 @@ console.log('💛💙💚¡RETOS!🧡💜💚');
 // 15 buzz
 // 15 fizzbuzz
 
-// for (let i = 1; i <= 100; i++) {
-//     console.log(i);
-// }
-
-// for (let i = 1; i <= 100; i++) {
-//     if (i % 3 === 0 && i % 5 === 0){
-//         console.log(i + ' FizzBuzz');
-//     } else if (i % 3 === 0){
-//         console.log(i + ' Fizz');
-//     } else if (i % 5 === 0){
-//         console.log(i + ' Buzz');
-//     } else {
-//         console.log(i);
-//     }
-// }
+for (i = 1; i <= 100; i++){
+    // console.log(i)
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i + ' FizzBuzz')
+    } else if (i % 5 == 0) {
+        console.log(i + ' Buzz')
+    } else if (i % 3 == 0) {
+        console.log(i + ' Fizz')
+    } else {
+        console.log(i)
+    }   
+}
