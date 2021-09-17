d2035f84d23ced693375e23eaaf0de7cb63bef65
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-09-16 22:41:07 -0500

    Realicé un ejercicio extra con la API de la NASA

--- 04_JS_Avanzado/03_HTTP_Requests/client.js
+++ 04_JS_Avanzado/03_HTTP_Requests/client.js
@@ -5,3 +5,9 @@ request.get('https://pokeapi.co/api/v2/pokemon/ditto', (err, res, body) => {
     const pokemon = JSON.parse(body);
     console.log(`Hola, mi nombre es ${pokemon.name}`);
 });
+
+request.get('https://api.nasa.gov/planetary/apod?api_key=SjkPhGcZdzRzqj6ZMkAbbVFanS6B4Wi9oaQKlKv0', (err, res, body) => {
+    console.log(res.statusCode);
+    const foto = JSON.parse(body);
+    console.log(`Esta foto se llama ${foto.title} y su descripción es: ${foto.explanation}`)
+});
