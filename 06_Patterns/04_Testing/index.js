3f55629134fc0840b37f5cd96217617e36f9e73c
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-09-23 20:55:07 -0500

    clase y ejercicios de testing

--- 06_Patterns/04_Testing/index.js
+++ 06_Patterns/04_Testing/index.js
@@ -1,4 +1,7 @@
 const sum = require ('./sum');
+const substract = require ('./substract')
 
 const resultado15 = sum(10, 5);
 console.log('Mi super resultado increíble: ' + resultado15);
+const resultado1 = substract(2, 1);
+console.log('Mi super resultado increíble: ' + resultado1);
