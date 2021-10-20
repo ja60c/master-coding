63a42fc090b9f837eed4a601c58c6082187f09ae
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-10-20 18:56:56 -0500

    clase API pg corregida

--- 07_Backend/05_API-pg/controllers/UsersController.js
+++ 07_Backend/05_API-pg/controllers/UsersController.js
@@ -1,81 +1,122 @@
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg');
+
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'postgres',
   password: '5qLja60c',
-  port: 5432,
-})
-pool.query('SELECT * FROM users', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+  port: 5432
+});
 
 const createUser = (req, res) => {
-    // Lógica de create
-    const { email, password } = req.body;
-  
-    if (!email || !password) {
-      return res.status(400).send({ message: 'Ingresar email y password' });
-    }
-  
-    const newUser = {
-      id: Math.floor(Math.random() * 100),
-      ...req.body
-    };
-  
-    return res.status(201).send({ message: 'Usuario creado!', newUser });
-  };
-  
-  const getAllUsers = (req, res) => {
-    // Lógica de read all
-    const users = [
-      { id: 1, email: 'pedrito@correo.com', password: 'abc123' },
-      { id: 2, email: 'juanito@correo.com', password: 'qwerty' }
-    ];
-    return res.status(200).send({ message: 'Estos son tus usuarios!', users });
-  };
-  
-  const getUserById = (req, res) => {
-    // Lógica de read one
-    const { id } = req.params;
-    const user = { id };
-    return res
-      .status(200)
-      .send({ message: `Este es tu usuario con el id: ${id}!`, user });
-  };
-  
-  const updateUser = (req, res) => {
-    // Lógica de update
-    const { id } = req.params;
-    const updatedUser = { id };
-    return res
-      .status(200)
-      .send({ message: `Actualizado el usuario con el id: ${id}!`, updatedUser });
-  };
-  
-  const updatePartialUser = (req, res) => {
-    // Lógica de partial update
-    const { id } = req.params;
-    const updatedUser = { id };
-    return res.status(200).send({
-      message: `Propiedad actualizada del usuario con el id: ${id}!`,
-      updatedUser
-    });
-  };
-  
-  const deleteUser = (req, res) => {
-    // Lógica de delete
-    const { id } = req.params;
-    delete id;
-    return res.status(204).send();
-  };
-  
-  module.exports = {
-    createUser,
-    getAllUsers,
-    getUserById,
-    updateUser,
-    updatePartialUser,
-    deleteUser
-  };
+  // Lógica de create
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send({ message: 'Ingresar email y password' });
+  }
+
+  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
+  pool
+    .query('INSERT INTO users (email, password) VALUES($1, $2) RETURNING *', [
+      email,
+      password
+    ])
+    .then(result => {
+      return res
+        .status(201)
+        .send({ message: 'Usuario creado!', user: result.rows[0] });
+    })
+    .catch(err => console.error(err.stack));
+};
+
+const getAllUsers = (req, res) => {
+  // Lógica de read all
+
+  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
+  pool
+    .query('SELECT * FROM users')
+    .then(result => {
+      return res
+        .status(200)
+        .send({ message: 'Estos son tus usuarios!', users: result.rows });
+    })
+    .catch(err => console.error(err.stack));
+};
+
+const getUserById = (req, res) => {
+  // Lógica de read one
+  const { id } = req.params;
+
+  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
+  pool
+    .query('SELECT * FROM users WHERE user_id = $1', [id])
+    .then(result => {
+      return res.status(200).send({
+        message: `Este es tu usuario con el id: ${id}!`,
+        user: result.rows[0]
+      });
+    })
+    .catch(err => console.error(err.stack));
+};
+
+const updateUser = (req, res) => {
+  // Lógica de update
+  const { id } = req.params;
+  const { email, password } = req.body;
+
+  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
+  pool
+    .query(
+      'UPDATE users SET email = $1, password = $2 WHERE user_id = $3 RETURNING *',
+      [email, password, id]
+    )
+    .then(result => {
+      return res.status(200).send({
+        message: `Actualizado el usuario con el id: ${id}!`,
+        user: result.rows[0]
+      });
+    })
+    .catch(err => console.error(err.stack));
+};
+
+const updatePartialUser = (req, res) => {
+  // Lógica de partial update
+  const { id } = req.params;
+  const { property, value } = req.body;
+
+  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
+  pool
+    .query(
+      `UPDATE users SET ${property} = '${value}' WHERE user_id = ${id} RETURNING *`
+    )
+    .then(result => {
+      return res.status(200).send({
+        message: `Parcialmente actualizado el usuario con el id: ${id}!`,
+        user: result.rows[0]
+      });
+    })
+    .catch(err => console.error(err.stack));
+};
+
+const deleteUser = (req, res) => {
+  // Lógica de delete
+  const { id } = req.params;
+
+  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
+  pool
+    .query('DELETE FROM users WHERE user_id = $1', [id])
+    .then(result => {
+      return res.status(204).send();
+    })
+    .catch(err => console.error(err.stack));
+};
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  updatePartialUser,
+  deleteUser
+};
