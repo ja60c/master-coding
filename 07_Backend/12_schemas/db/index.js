8e12c2318be87524e935ea226464a20a7fc29bb2
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-12-09 19:34:43 -0600

    Refactorización de archivos.

--- 07_Backend/12_schemas/db/index.js
+++ 07_Backend/12_schemas/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGODB_DATABASE_USERNAME}:${process.env.MONGODB_DATABASE_PASSWORD}@cluster0.ol6yb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGODB_DATABASE_USERNAME}:${process.env.MONGODB_DATABASE_PASSWORD}@cluster0.n6und.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
   )
   .then(() => console.log('Conectado a la base de datos'))
   .catch(err => console.log('Error conectando', err.message));
--- 07_Backend/12_schemas/index.js
+++ 07_Backend/12_schemas/index.js
@@ -1,38 +1,5 @@
-const mongoose = require('mongoose');
+const app = require('./app');
+const PORT = 3000;
 
-const db = require('./db');
-const { BlogModel } = require('./models');
-
-// CREATE
-BlogModel.create({
-  title: 'Un día increíble para aprender Schemas',
-  author: 'Pedro A. González',
-  body:
-    'Hoy me la pasé increíble en la clase del master g7 ya que estoy feliz de aprender schema'
-})
-  .then(newPost => console.log('Post publicado!', newPost))
-  .catch(error => console.log(error));
-
-// GET ALL
-BlogModel.find()
-  .then(posts => console.log('MIS POSTS', posts))
-  .catch(error => console.log(error));
-
-// GET ONE
-BlogModel.findById('61aec6c7279483496602e4e2')
-  .then(postEncontrado => console.log('POST ENCONTRADO', postEncontrado))
-  .catch(error => console.log(error));
-
-// UPDATE
-BlogModel.findByIdAndUpdate(
-  '61aec6c7279483496602e4e2',
-  { title: 'OTRO día increíble para aprender Schemas' },
-  { new: true }
-)
-  .then(updatePost => console.log('POST ACTUALIZADO', updatePost))
-  .catch(error => console.log(error));
-
-// DELETE
-BlogModel.findByIdAndDelete('61a98a48df12e8684ee55e27')
-  .then(() => console.log('POST BORRADO'))
-  .catch(error => console.log(error));
+// LISTENER
+app.listen(PORT, () => console.log('App running on http://localhost:' + PORT));
